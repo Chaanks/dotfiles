36e3e9b19c25eb8c4363d95c0ca3d49d29a1e6a2
Chaanks <jarod8405@gmail.com>
2022-03-16 16:13:06 +0100

    clean old files

--- .config/xrandr/dual_monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# set up the two monitors for bspwm
-second_monitor=$(xrandr --query | grep 'HDMI-0')
-if [[ $second_monitor = *connected* ]]; then
-    xrandr --output DP-0 --primary --mode 2560x1440 --rotate normal --output HDMI-0 --mode 2560x1440 --rotate normal --above DP-0
-fi
--- .config/xrandr/test.sh
+++
@@ -1,4 +0,0 @@
-if [ -x $HOME/.config/xrandr/dual_monitor.sh ];then
-    echo "Hello"
-    $HOME/.config/xrandr/dual_monitor.sh
-fi
